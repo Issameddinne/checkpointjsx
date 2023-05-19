97151a9ac1c9417cff353cba2529cd8a8c90b3ed
issameddinne <issameddinnel@gmail.com>
2023-05-19 13:12:04 +0200

    checkpoint Jsx

---
+++ src/Component/Profile/Address.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Address = () => {
+    return (
+        <div>
+            <p>Adresse: 30 rue bayrouth soliman Riadh Nabeul, Tunisie</p>
+        </div>
+    );
+}
+
+export default Address;
---
+++ src/Component/Profile/FullName.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const FullName = () => {
+    return (
+        <div class = "text-center ">
+            <p>Nom: Laabidi</p>
+            <p>Prénom: Issameddinne</p>
+
+        </div>
+    );
+}
+
+export default FullName;
